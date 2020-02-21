cf8dd16548091d85d21611fe9fa4208318dacad0
root <root@u37b5d23162e85c.ant.amazon.com>
2020-02-21 01:46:18 -0500

    Import modeule

--- versa5g/demo.v
+++ versa5g/demo.v
@@ -1,31 +1,20 @@
+`include "KeyDecoder.v"
+
+
 module top(input clk, output [7:0] led, output [13:0] disp);
 
-    
-    localparam div_n = 25;
-    reg clk_div;
-    reg [div_n-1:0] div_ctr; 
-    
-    always @(posedge clk) begin
-        div_ctr <= div_ctr + 1'b1;
-        clk_div <= (div_ctr == 0);
-    end
-    
-    `include "pattern.vh"
-    `include "KeyDecoder.v"
-
-    reg [$clog2(pat_len):0] pat_ctr;
-    always @ ( posedge clk ) begin
-        if (clk_div) begin
-            if (pat_ctr == 2*pat_len - 1)
-                pat_ctr <= 0;
-            else
-                pat_ctr <= pat_ctr + 1'b1;
-        end
-    end
-    
-    assign led = {clk, ~pat_ctr};
-    assign disp = pat_ctr[0] ? 14'h3FFF : ~(display_pat[pat_ctr[$clog2(pat_len):1]]);
-
+    KeyDecoder decoder_1(
+	.reset(1),
+	.clock(clk),
+	.io_input_0(0),
+        .io_input_1(0),
+        .io_input_2(0),
+        .io_input_3(0),
+        .io_input_4(0),
+        .io_input_5(0),
+        .io_input_6(0),
+        .io_input_7(0),
+    );
 
 endmodule
 
